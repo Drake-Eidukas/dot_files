f2d8bb0c6f8d243f3491f677648941b68c8a4c52
= <=>
2018-02-21 10:48:10 -0600

    Added helper script to symlink dot files into place.

---
+++ symlink.sh
@@ -0,0 +1,3 @@
+ln -s ~/dot_files/.zshrc ~/.zshrc
+ln -s ~/dot_files/.vimrc ~/.vimrc
+ln -s ~/dot_files/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json
